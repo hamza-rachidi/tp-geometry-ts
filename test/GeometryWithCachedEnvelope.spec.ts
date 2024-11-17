b2e8e6a273d9d3cc857e3015a26b493fece56c28
rondinasia <60042180+rondinasia@users.noreply.github.com>
2024-11-17 14:25:45 +0100

    Optimisation de quelques tests

--- test/GeometryWithCachedEnvelope.spec.ts
+++ test/GeometryWithCachedEnvelope.spec.ts
@@ -10,17 +10,43 @@ import Point from "../src/Point";
 
 describe("testeur de la classe GeometryWithCachedEnvelope", () => {
 
-    it("tester la fonctions get enveloppe et autres tests", () => {
+    it("tester la fonctions get enveloppe ", () => {
         let geom: Geometry = new Point([3.0,3.0]);
         geom = new GeometryWithCachedEnvelope(geom);
+
+        // test getEnvelope
         const a = geom.getEnvelope();
         const b = geom.getEnvelope();
         
         expect(a).to.equal(b);
+        
+    });
+
+    it("test simultané de clonage et translation", () => {
+        let geom: Geometry = new Point([3.0,3.0]);
+        geom = new GeometryWithCachedEnvelope(geom);
+        const copy = geom.clone();
+        geom.translate(1.0,1.0);
+        expect(copy.asText()).to.equal("POINT(3 3)"); 
+        expect(geom.asText()).to.equal("POINT(4 4)"); 
+        
+    });
+
+    it("test autres fonctions de la classe", () => {
+        let geom: Geometry = new Point([3.0,3.0]);
+        geom = new GeometryWithCachedEnvelope(geom);
         expect(geom.getType()).to.equal("Point");
         expect(geom.isEmpty()).to.be.false;
-        //expect(geom.clone()).to.deep.equal(geom.clone());
-        expect(geom.asText()).to.deep.equal("POINT(3 3)");
+        expect(geom.asText()).to.equal("POINT(3 3)"); 
+        
+    });
+
+    it("test de l'acceptation du visiteur ", () => {
+        let geom: Geometry = new Point([3.0,3.0]);
+        geom = new GeometryWithCachedEnvelope(geom);
+        const visitor = new WktVisitor();
+        geom.accept(visitor);
+        expect(visitor.getResult()).to.equal("POINT(3 3)"); 
         
     });
 
---
+++ test/NonvalidGeometry.spec.ts
@@ -0,0 +1,28 @@
+import "mocha";
+import { expect } from "chai";
+import Point from "../src/Point";
+import EnvelopeBuilder from "../src/EnvelopeBuilder";
+import NonvalidGeometry from "../src/NonvalidGeometry";
+import WktVisitor from "../src/WktVisitor";
+
+describe("Nonsense test juste pour tester autre type de Géométrie dans Wkt", () => {
+
+    it("test classe", () => {
+        const x = new NonvalidGeometry();
+        expect(x.getType()).to.equal("None");
+        expect(x.isEmpty()).to.equal(true);
+        expect(x.translate(0,0)).to.equal(undefined);
+        expect(x.clone()).to.equal(undefined);
+        expect(x.asText()).to.equal("Invalid Geometry");
+        expect(x.getEnvelope()).to.equal(undefined);
+
+        const visitor = new WktVisitor();
+        x.accept(visitor);
+        expect(visitor.getResult()).to.equal("None"); 
+
+
+
+    });
+    
+});
+
